e7decf94f4b0a9648b67b8dc0f3df09c07eebf24
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-01-29 18:00:37 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -3,9 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/main.css">
     <title>Document</title>
 </head>
 <body>
-    hello this is the index.php file
+    <?php get_header();?>
+
+    <h1>IDM250</h1>
+
+    <p>hello this is the index.php file</p>
+
+    <?php get_footer();?>
+
 </body>
 </html>
