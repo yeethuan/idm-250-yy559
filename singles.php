77c1a07c448f4f77c4101ef9aeaa3c8e92d592c1
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-02-04 17:33:09 -0500

    Update singles.php

---
+++ singles.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/main.css">
+    <title>Singles</title>
+</head>
+<body>
+    <?php get_header();?>
+
+    <h1>IDM250</h1>
+
+    <p>hello this is the singles.php file</p>
+
+    <?php get_footer();?>
+
+</body>
+</html>
