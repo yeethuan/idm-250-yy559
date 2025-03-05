ce8b05e791a61684b4b6883065420f9f620838cd
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-03-04 23:07:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/main.css">
-    <title>Index</title>
-</head>
-<body>
-    <?php get_header();?>
+<?php get_header(); ?>
 
-    <h1>IDM250</h1>
+<h1>IDM250</h1>
+<p>hello this is the index.php file</p>
 
-    <p>hello this is the index.php file</p>
-
-    <?php get_footer();?>
-
-</body>
-</html>
+<?php get_footer(); ?>
