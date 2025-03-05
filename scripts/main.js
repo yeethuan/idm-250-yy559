62357d4dcf61fe5056318cc14c9cf7c9c78653c9
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-03-05 00:35:06 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-main.js
+//side panel js start
+ 
+            function openNav() {
+                document.getElementById("mySidebar").style.width = "250px";
+            }
+    
+            function closeNav() {
+                document.getElementById("mySidebar").style.width = "0";
+            }
+    
+            function toggleNav() {
+                var sidepanel = document.getElementById("mySidepanel");
+                var topnav = document.getElementById("myTopnav");
+    
+                if (sidepanel.style.width === "250px") {
+                    sidepanel.style.width = "0";
+                    topnav.style.display = "block";
+                } else {
+                    sidepanel.style.width = "250px";
+                    topnav.style.display = "none";
+                }
+                
+            }
