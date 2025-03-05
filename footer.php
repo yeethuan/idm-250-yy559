d09c5e929f22030e5af62dfbf8d639f5de76633f
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-03-04 23:35:31 -0500

    Update footer.php
    updated footer

--- footer.php
+++ footer.php
@@ -1,6 +1,16 @@
 
     <footer>
-            Yi Huan Yang | yihuan.yang@drexel.edu | linkedin.com/in/yihuanyang/
+        <div class="social-links">
+                <a href="https://www.linkedin.com/in/yihuanyang/" target="_blank" aria-label="LinkedIn">
+                    <i class="fab fa-linkedin"></i> 
+                </a>
+                <a href="mailto:yihuan.yang@drexel.edu" target="_blank" aria-label="Email">
+                    <i class="fas fa-envelope"></i>
+                </a>
+                <a href="https://github.com/yeethuan" target="_blank" aria-label="GitHub">
+                    <i class="fab fa-github"></i>
+                </a>
+        </div>
     </footer>
     
 </body>
