5caf8b870bcc1d6636bf5234c540268047c2919e
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-03-05 02:43:51 -0500

    404 error

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>    
+
+<h1>404 Error</h1>
+<img class=error width=200 height=200 src="<?php echo get_stylesheet_directory_uri(); ?>/media/oops.jpg" alt="error">
+
+<?php get_footer(); ?>    
