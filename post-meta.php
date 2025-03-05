af540195714aff06663a59149abc8bf2108dae7c
yeethuan <146368491+yeethuan@users.noreply.github.com>
2025-03-05 01:29:21 -0500

    Create post-meta.php

---
+++ post-meta.php
@@ -0,0 +1,6 @@
+<div class="post-meta">
+    <p><strong>Published on:</strong> <?php echo get_the_date(); ?></p>&nbsp;|&nbsp;
+    <p><strong>Author:</strong> <?php the_author_posts_link(); ?></p>&nbsp;|&nbsp;
+    <p><strong>Published on:</strong> <?php the_category(','); ?></p>&nbsp;|&nbsp;
+    <p><strong>Published on:</strong> <?php the_tags('',',',''); ?></p>&nbsp;|&nbsp;
+</div>
